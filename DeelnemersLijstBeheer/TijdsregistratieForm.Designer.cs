100edd285db470cf20e69d1c5e5c92b4f4322ff7
dieter vermander <vermanderdieter@hotmail.com>
2019-07-15 13:09:01 +0200

    made tijdregistratieform code ready for registratie weergeven.

--- DeelnemersLijstBeheer/TijdsregistratieForm.Designer.cs
+++ DeelnemersLijstBeheer/TijdsregistratieForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button2
@@ -39,15 +40,28 @@
             this.button2.TabIndex = 9;
             this.button2.Text = "Weergeven";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(368, 344);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.ListBox1_SelectedValueChanged);
             // 
             // TijdsregistratieForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(389, 88);
+            this.ClientSize = new System.Drawing.Size(389, 440);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Name = "TijdsregistratieForm";
             this.Text = "TijdsregistratieForm";
+            this.Load += new System.EventHandler(this.TijdsregistratieForm_Load);
             this.ResumeLayout(false);
 
         }
@@ -55,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- DeelnemersLijstBeheer/TijdsregistratieForm.cs
+++ DeelnemersLijstBeheer/TijdsregistratieForm.cs
@@ -12,9 +12,38 @@ namespace DeelnemersLijstBeheer
 {
     public partial class TijdsregistratieForm : Form
     {
+        public string tijd { get; set; }
         public TijdsregistratieForm()
         {
             InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            TijdsRegistratieWeergevenForm weergevenForm = new TijdsRegistratieWeergevenForm();
+            weergevenForm.ShowDialog();
+        }
+
+        private void TijdsregistratieForm_Load(object sender, EventArgs e)
+        {
+            using (var ctx = new OpleidingDatabaseContext())
+            {
+                var tijdsreg = ctx.TijdRegistraties;
+                foreach (var item in tijdsreg)
+                {
+                    listBox1.Items.Add(item.DateTime);
+                }
+            }
+        }
+        public string GetSelectedItem()
+        {
+            string selected = listBox1.SelectedItem.ToString();
+            return selected;
+        }
+
+        private void ListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            tijd = GetSelectedItem();
         }
     }
 }
